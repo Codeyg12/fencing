198e2b8a42da25ebc6a5615210eb59f30e66b2f6
Codeyg12 <codey.gallup@gmail.com>
2023-03-13 19:15:59 -0600

    erased background, adding navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 function App() {
   return (
     <>
-        <header className="background">
+        <header>
+          {/* <div className="background"></div> */}
           <h1>Triple Striping Construction</h1>
+          <ul className="link-list">
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Testimonials</li>
+            <li>Request a Quote</li>
+          </ul>
         </header>
     </>
   );
