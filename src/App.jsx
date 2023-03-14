34644e9c005d63fd4eeb713d322994b55ba40555
Codeyg12 <codey.gallup@gmail.com>
2023-03-13 19:19:44 -0600

    put into a nav element and changed from font size to color change

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ function App() {
         <header>
           {/* <div className="background"></div> */}
           <h1>Triple Striping Construction</h1>
-          <ul className="link-list">
+          <nav className="link-list">
             <li>Home</li>
             <li>About Us</li>
             <li>Testimonials</li>
             <li>Request a Quote</li>
-          </ul>
+          </nav>
         </header>
     </>
   );
