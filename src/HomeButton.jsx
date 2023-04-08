daf216bd2311ef6d59af4948c2f2065acde89ad3
Codeyg12 <codey.gallup@gmail.com>
2023-04-08 16:14:25 -0600

    changed display location of enter and home buttons

--- src/HomeButton.jsx
+++ src/HomeButton.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export default function HomeButton({ buttonName }) {
     return (
-        <Link to="/home">
+        <Link to="/home" className="home-btn">
         {buttonName ?? 'Home'}
         </Link>
     )
